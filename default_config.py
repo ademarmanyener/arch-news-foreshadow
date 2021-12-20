4054e54a4377d4c549a1aa97cfc176c2d47a750c
adewww <lovisestdeus@tutanota.com>
2021-12-20 22:31:24 +0300

    new config options && termcolor && fetch data from archive

--- default_config.py
+++ default_config.py
@@ -1,7 +1,10 @@
 SETTINGS = {
-        'url': 'https://archlinux.org/', # don't touch here
+        'url_homepage': 'https://archlinux.org/',
+        'url_archive': 'https://archlinux.org/news/',
         'headers': {
             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"
         },
+        'source': 'homepage', # it can be either 'homepage' or 'archive'
         'parser': 'html.parser',
+        'bottom_up': False,
 }
--- foreshadow.py
+++ foreshadow.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
+from termcolor import colored, cprint
 import requests
 import os
+import subprocess
 
 if os.path.isfile('config.py'):
     from config import SETTINGS
@@ -10,10 +12,15 @@ else:
     from default_config import SETTINGS
 
 MY_HEADERS = SETTINGS['headers']
-R = requests.get(url=SETTINGS['url'], headers=MY_HEADERS)
+if SETTINGS['source'] == 'homepage': R = requests.get(url=SETTINGS['url_homepage'], headers=MY_HEADERS)
+elif SETTINGS['source'] == 'archive': R = requests.get(url=SETTINGS['url_archive'], headers=MY_HEADERS)
 SOUP = BeautifulSoup(R.content, SETTINGS['parser'])
 
-def show_news():
+PRINT_BOLD = lambda x: cprint(x, attrs={'bold'})
+PRINT_BOLD_RED = lambda x: cprint(x, 'red', attrs={'bold'})
+PRINT_BOLD_GREEN = lambda x: cprint(x, 'green', attrs={'bold'})
+
+def show_news_homepage():
     news = SOUP.find('div', attrs={'id': 'news'})
     news_len = len(news.find_all('h4'))
     news_titles = news.find_all('h4')
@@ -25,9 +32,10 @@ def show_news():
         news_timestamp = news_timestamps[new_loop].text
         news_article_content = news_article_contents[new_loop]
 
-        print('[ ' + news_timestamp + ' ] ' + news_title)
-        for _ in range(len('[ ' + news_timestamp + ' ] ' + news_title)):
-            print('=', end='')
+        output_text = colored('[' + news_timestamp + '] ' + news_title, 'white', attrs={'bold'})
+        print(output_text)
+        for _ in range(len(output_text)):
+            print(colored('=', 'white', attrs={'bold'}), end='')
         print('')
 
         for article in news_article_content.find('p'):
@@ -35,4 +43,31 @@ def show_news():
 
         print('\n')
 
-if __name__ == '__main__': show_news()
+def show_news_archive():
+    news = SOUP.find('table', attrs={'id': 'article-list', 'class': 'results'}).find('tbody').find_all('tr')
+    news_len = len(news)
+
+    if SETTINGS['bottom_up']: news.reverse()
+    
+    for new_loop in range(news_len):
+        selected_news = news[new_loop]
+        selected_news_title = selected_news.find_all('td')[1].find('a').text
+        selected_news_published_date = selected_news.find_all('td')[0].text
+        selected_news_author = selected_news.find_all('td')[2].text
+        output_text = colored('[{}]'.format(selected_news_published_date), 'white', attrs={'bold'}) + ' {} - {}'.format(selected_news_title, selected_news_author) 
+        print(output_text)
+        for _ in range(len(output_text)): print(colored('', 'white', attrs={'bold'}), end='')
+        print('')
+
+def ask_update():
+    try:
+        get_answer = input(colored('::', 'blue', attrs={'bold'}) + ' ' + colored('Do you want to update your system right now? [y/N]: ', 'white', attrs={'bold'}))
+        if get_answer.lower() == 'y': result_process = subprocess.run(['sudo', 'pacman', '-Syu'])
+        else: PRINT_BOLD_RED('Cancelled.')
+    except KeyboardInterrupt: PRINT_BOLD_RED('Keyboard Interrupt.')
+
+if __name__ == '__main__':
+    result_process = subprocess.run(['clear'])
+    if SETTINGS['source'] == 'homepage': show_news_homepage()
+    elif SETTINGS['source'] == 'archive': show_news_archive()
+    ask_update()
